c1578e445e217b259a5f88e68b8dabd29451e20d
masacoco <masaonakama0329@gmail.com>
2021-12-12 19:02:23 +0900

    [finish]practice 4

---
+++ DWCpractice4.java
@@ -0,0 +1,15 @@
+
+public class DWCpractice4 {
+
+	public static void main(String[] args) {
+		final int Lucky_Number = 29;
+		String str;
+		str = "Masao Nakama";
+		System.out.println(str);
+		str = "やればできる";
+		System.out.println(str);
+		System.out.println(Lucky_Number);
+
+	}
+
+}
