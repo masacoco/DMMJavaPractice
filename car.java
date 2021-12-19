e6c4a8c9965932c83c84d643b6d68717ca1efe1d
masacoco <masaonakama0329@gmail.com>
2021-12-19 16:24:51 +0900

    [adjust session9

--- car.java
+++ car.java
@@ -1,9 +1,22 @@
+package practice;
 
-public class car {
+public class Car {
+    // 燃費（Km/L）
+    private double fuelCost;
+    // 残量（L）
+    private double fuelAmount;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+    public Car(double fuelCost, double fuelAmount) {
+        this.fuelCost = fuelCost;
+        this.fuelAmount = fuelAmount;
+    }
 
-	}
+    public void move(int km) {
+        System.out.println(km + " km 走ります");
+        this.fuelAmount -= (km / fuelCost);
+    }
 
+    public double getFuelAmount() {
+        return this.fuelAmount;
+    }
 }
